6e6027d2ed9eec68659a00b47383a3e84f94e520
Meisam Booshehri <me.booshehri@gmail.com>
2015-02-19 23:55:07 +0100

    My second commit consisting of some updates

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 
 # This R code has been produced by Meisam Booshehri
 # The code has been written exacly according to the format of sample code in the assignment.
+
 # The code consists of two functions in which the "superassignment" operator, i.e. "<<-", has been used
 # in conjunction with the lexical scope, where an environment stores "state" for two functions 
 # that modify the "state" by using superassignment.By "state", I am refering to the same Cache
@@ -17,9 +18,9 @@
 
 # MakeCacheMatrix: In this function you can see a block-structured approach, i.e. there you can see some
 # some functions defined in another function. 
-# The set function will update the cache, i.e. "x" and "m". By setting a new matrix for computing the 
-# the transpose, x will be updated by being assigned to the new matrix and m becomes NULL which is a sign 
-# for the cacheSove function, meaning that the cache should be updated and the new transpose matrix should
+# The set function will update the cache, i.e. "x" and "m". By setting a new matrix for computing  
+# the inverse, x will be updated by being assigned to the new matrix and m becomes NULL which is a sign 
+# for the cacheSove function, meaning that the cache should be updated and the new inverse matrix should
 # be assigned to "m".
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -29,28 +30,28 @@ makeCacheMatrix <- function(x = matrix()) {
     m<<-NULL
   }
   get<-function() x
-  setmatrix<-function(solve) m<<- solve
-  getmatrix<-function() m
+  setMat<-function(solve) m<<- solve
+  getMat<-function() m
   list(set=set, get=get,
-       setmatrix=setmatrix,
-       getmatrix=getmatrix)
+       setMat=setMat,
+       getMat=getMat)
 }
 
 
-# In the following function, i.e. cacheSolve, we keep track of any modification made to the the variable "m"
-# If "m" is not NULL, it means that cache is up-to-date. But in case of setting a new matrix for computing
-# the transpose, "m" becomes NULL and part of cache, i.e. "m",will be updated by cacheSolve.It will hold
-# the transpose Matrix.
+# The following function returns the inverse of "x". Actually cacheSolve keeps track of any modification 
+# made to the the variable "m". If "m" is not NULL, it means that cache is up-to-date. But in case of 
+# setting a new matrix for computing the inverse, "m" becomes NULL and part of cache, i.e. "m",will
+# be updated by cacheSolve."m" will hold the inverse Matrix.
 
 cacheSolve <- function(x=matrix(), ...) {
-  m<-x$getmatrix()
+  m<-x$getMat()
   if(!is.null(m)){
     message("getting cached data")
     return(m)
   }
   matrix<-x$get()
   m<-solve(matrix, ...)
-  x$setmatrix(m)
+  x$setMat(m)
   m
 }
 
